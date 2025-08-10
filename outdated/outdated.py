897888813a257e8e96f3e81b3129435104fb5348
unknown <robertaandrade0916@gmail.com>
2025-08-09 21:35:48 -0300

    atualização de código

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -1,31 +1,57 @@
-# pergunta ao user o pedido, retornando  sempre o total atual
-# encerra o programa após pressionar Ctrl + d
-# https://docs.python.org/3/library/stdtypes.html#mapping-types-dict
+# ISO 8601 norma internacional para a representação de datas e horas, formatado (YYYY-MM-DD)
+# cs50: solicita ao user uma data (Anno Domini - BC), formatado mm/dd/yyyy or month D, YYYY.
+# ----- imprime a mesma data no formato yyyy-mm-dd.
 
-menu = {
-    "baja taco": 4.25,
-    "burrito": 7.50,
-    "bowl": 8.50,
-    "nachos": 11.00,
-    "quesadilla": 8.50,
-    "super burrito": 8.50,
-    "super quesadilla": 9.50,
-    "taco": 3.00,
-    "tortilla salad": 8.00
-}
+# dict.get: pega o valor da chave no dicionário se existir, se não retorna None ou um valor padrão se passar
+# ex f"{var:02}": transforma número em str com pelo menos 2 dígitos, adicionando 0's na frente se necessário
 
-total = 0.0
+meses = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
 
 while True:
-    try:
-        item = input("Item: ").strip().lower()
-    except EOFError:
-        break
+    data = input("Data: ").strip()
 
-    if item not in menu:
-        continue
+    if "/" in data:
+        date_slice = data.split("/")
+        if len(date_slice) == 3: # verifica se tem 3 elemts
+            mes, dia, ano = date_slice
 
-    total = total + menu[item]
-    print(f"Total: ${total:.2f}")
+            if mes.isdigit() and dia.isdigit() and ano.isdigit():
+                mes = int(mes)
+                dia = int(dia)
+                ano = int(ano)
+                if 1 <= mes <= 12 and 1 <= dia <= 31 and ano > 0:
+                    print(f"{ano:04}-{mes:02}-{dia:02}")
+                    break
 
-print(f"Total: ${total:.2f}")
+    else:
+        if "," not in data:
+            continue
+
+        try:
+            date_slice = data.replace(",", "").split()
+            if len(date_slice) == 3:
+                mes_extenso, dia, ano = date_slice
+
+                if mes_extenso in meses and dia.isdigit() and ano.isdigit():
+                    mes = meses.index(mes_extenso) + 1 # p/ contar a partir de 1, não 0
+                    dia = int(dia)
+                    ano = int(ano)
+
+                    if 1 <= dia <= 31 and ano > 0:
+                        print(f"{ano:04}-{mes:02}-{dia:02}")
+                        break
+        except:
+            pass
